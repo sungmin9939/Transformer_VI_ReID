9117785d62731a8e22f785706e83d60048000618
sungmin <sungmin9939@naver.com>
2021-12-23 17:16:18 +0900

    modality aware transformer

--- test10.py
+++ test10.py
@@ -1,5 +1,5 @@
 import torch
-from models.transformers_MA import TransformerEncoder, PatchEmbedding
+from models.transformers_MA import TransformerEncoder, PatchEmbedding, Trans_VIReID
 from PIL import Image
 import argparse
 
@@ -7,14 +7,28 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--dataset',default='RegDB')
 parser.add_argument('--lr', default=0.1, type=float)
 parser.add_argument('--optim',default='sgd', type=str)
-parser.add_argument('--batch_size',default=8)
+
+
+
+parser.add_argument('--depth', default=5)
+parser.add_argument('--dim', default=768)
+parser.add_argument('--heads', default=4)
+parser.add_argument('--mlp_ratio', default=4)
+parser.add_argument('--drop_rate', default=0)
+parser.add_argument('--num_classes', default=412)
 parser.add_argument('--img_size',default=128)
+parser.add_argument('--patch_size',default=16)
+parser.add_argument('--in_channel',default=3)
+parser.add_argument('--is_train',default=True)
+parser.add_argument('--batch_size',default=8)
+parser.add_argument('--margin',default=0.5)
 
 
 opt = parser.parse_args()
 
 device = torch.device('cuda:0')
-model = TransformerEncoder(5, 768, 4).to(device)
+model = Trans_VIReID(opt)
+
 
 img = Image.open('./cat.jpg')
 img2 = Image.open('./dog.jpg')
