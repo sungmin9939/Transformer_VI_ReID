925b4a938aa83313d74e51628c763ce98a91b5e1
sungmin9939 <sungmin9939@naver.com>
2021-12-29 00:14:12 +0900

    model modifying

--- test4.py
+++ test4.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-
+from einops.layers.torch import Rearrange
 conv = nn.Conv2d(3, 768, kernel_size=(16,16), stride=(8,8))
 x = torch.randn(1,3,256,128)
 output = conv(x).flatten(2).transpose(1,2)
@@ -10,7 +10,7 @@ print(output.shape)
 
 conv2 = nn.ConvTranspose2d(768,3,kernel_size=(16,16),stride=(8,8))
 to_img = nn.Sequential(
-    
+    Rearrange('b (h w) c -> b c h w', h=(256-8)/8)
 )
 input = conv2(output)
 
