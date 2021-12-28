68201d01219f588525aba86272c9bdfe2e18fb05
sungmin <sungmin9939@naver.com>
2021-12-28 16:25:45 +0900

    transformers

--- test10.py
+++ test10.py
@@ -1,9 +1,14 @@
 import torch
-from models.transformers_MA import TransformerEncoder, PatchEmbedding, Trans_VIReID
-from PIL import Image
-import argparse
-import torch.nn as nn
+from transformers import ViTFeatureExtractor, ViTModel
 
-a = nn.Parameter(torch.ones(5,5))
-b = torch.randn(5,5)
-print(a+b)
+model = ViTModel.from_pretrained('google/vit-base-patch16-224-in21k')
+
+input = torch.randn(1,3,128,256)
+input2 = torch.randn(1,3,224,224)
+print(model.embeddings.rgb_embeddings.shape)
+
+
+output = model(input, interpolate_pos_encoding=True, modal=1)
+output2 = model(input2)
+print(output.last_hidden_state.shape)
+print(output2.last_hidden_state.shape)
